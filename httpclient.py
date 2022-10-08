818cc0ceb3f507862b12f44f45eb23d42b0988e6
Eric Weber <edweber@cs.ualberta.ca>
2022-10-07 22:42:57 -0600

    Commit assignment

--- httpclient.py
+++ httpclient.py
@@ -20,9 +20,9 @@
 
 import sys
 import socket
-import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -33,7 +33,7 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    USER_AGENT = "assign2_edweber/1.0"
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,17 +41,20 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        try:
+            return int(data.split("\r\n")[0].split()[1])
+        except Exception:
+            raise Exception("non-valid response code")
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        return data.split("\r\n\r\n")[0]
 
     def get_body(self, data):
-        return None
-    
+        return data.split("\r\n\r\n")[1]
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+  
     def close(self):
         self.socket.close()
 
@@ -66,15 +69,75 @@ class HTTPClient(object):
             else:
                 done = not part
         return buffer.decode('utf-8')
+    
+    def parse_request_url(self, url):
+        """
+        @param: url: the full url requested
+
+        @return: Tuple: a tuple containing the hostm followed by the resource
+                requested
+        """
+        res = urllib.parse.urlparse(url)
+        if res.scheme != "http":
+            raise Exception("url scheme invalid: ", res.scheme)
+        host = res.netloc
+        port = 80
+        resource = res.path or "/"
+        if ":" in res.netloc:
+            host, port = res.netloc.split(":")
+        return (host, int(port), resource)
+
+    def parse_response(self, resp):
+        try:
+            code = self.get_code(resp)
+            headers = self.get_headers(resp) + "\r\n"
+            body = self.get_body(resp)
+        except Exception:
+            code = 500
+            headers = ""
+            body = ""
+        finally:
+            return (code, headers, body)
+
+    def make_headers(self, host, resource, post=False, content_length = None):
+        method = "POST" if post else "GET"
+        request = f"{method} {resource} HTTP/1.1\r\n" \
+                  f"Host: {host}\r\n" \
+                  "Connection: close\r\n" \
+                  "Accept: */*\r\n" \
+                  "Accept-Encoding: */*\r\n" \
+                 f"User-Agent: {self.USER_AGENT}\r\n"
+        if post:
+            request += "Content-Type: application/x-www-form-urlencoded\r\n"
+        if not content_length == None:
+            request += f"Content-Length: {content_length}\r\n"
+        request += "\r\n"
+        return request
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        host, port, resource = self.parse_request_url(url)
+        self.connect(host, port)
+        request = self.make_headers(host, resource)
+        self.sendall(request)
+        code, headers, body = self.parse_response(self.recvall(self.socket))
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+
+        host, port, resource = self.parse_request_url(url)
+        try:
+            post_body =  urllib.parse.urlencode(args)
+        except Exception:
+            post_body = ""
+        content_length = len(post_body)
+        self.connect(host, port)
+        request = self.make_headers(host, resource, post=True, content_length=content_length)
+        request += post_body
+        request += "\r\n\r\n"
+        self.sendall(request)
+        code, headers, body = self.parse_response(self.recvall(self.socket))
+        self.close()
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
